7ef63a8045e197a954d7ef602d5bce6a4c48bf5f
emthaa <110277522+emthaa@users.noreply.github.com>
2024-02-14 01:46:10 +0000

    filtered pokemon list

--- src/components/PokemonApiData.jsx
+++ src/components/PokemonApiData.jsx
@@ -19,13 +19,36 @@ function PokemonList() {
       }
     };
 
+    const PokemonObject = (name,imageURL) => {
+    
+      const newObject = {
+        name: name,
+        imageURL: imageURL
+      }
+      
+      return newObject
+   
+    }
+
+    const filterPokemonList = (pokemonData) =>{
+      console.log(pokemonData)
+      const newPokemonList = pokemonData.map(pokemon => ({
+        name: pokemon.name,
+        imageURL: pokemon.sprites.front_default
+      }));
+      
+      setPokemonList(newPokemonList);
+    }
+
     const fetchPokemonData = async () => {
       try {
         const initialData = await fetchData("https://pokeapi.co/api/v2/pokemon?offset=0&limit=12");
         if (initialData) {
           const pokemonDataPromises = initialData.results.map(pokemon => fetchData(pokemon.url));
           const pokemonData = await Promise.all(pokemonDataPromises);
-          setPokemonList(pokemonData);
+          filterPokemonList(pokemonData)
+          
+          
         }
       } catch (error) {
         console.error('Error fetching Pokemon data:', error);
@@ -35,7 +58,7 @@ function PokemonList() {
     fetchPokemonData();
   }, []);
 
-  console.log(pokemonList); 
+  console.log(pokemonList)
   
 }
 
